bec3e48a316e4dd8e39d4f86dfa9ddb9fca64273
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-01-02 21:57:38 +0100

    Added header and global styling

--- web/src/components/Layout/layout.js
+++ web/src/components/Layout/layout.js
@@ -1,9 +1,13 @@
 import React from "react"
 
+import Header from "../header/header"
+
+import "../../styles/site.scss"
 import "./layout.scss"
 
 const Layout = ({ children }) => (
   <>
+    <Header siteTitle={"Site title"} />
     <div className={"content"}>{children}</div>
   </>
 )
