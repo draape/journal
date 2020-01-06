d17ba030be4a7af0777fed59d41ca5d421a69c2a
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-01-06 22:34:14 +0100

    Added likes and comments to sanity schema

---
+++ sanity/schemas/postImage.js
@@ -0,0 +1,15 @@
+export default {
+  name: "postImage",
+  title: "Image",
+  type: "object",
+  fields: [
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true
+      }
+    }
+  ]
+};
