a4bb46681b406c86bd3ea7f4a7308464ddfecca2
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-01-26 00:10:21 +0100

    Added post component

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -4,36 +4,13 @@ import { graphql } from "gatsby"
 import { mapEdgesToNodes } from "../lib/helpers"
 
 import Layout from "../components/layout/Layout"
-import Byline from "../components/byline/byline"
-import ImageCarousel from "../components/image-carousel/image-carousel"
-import Controls from "../components/controls/controls"
-import Likes from "../components/likes/likes"
-import PostBody from "../components/post-body/post-body"
-import CommentList from "../components/comment-list/comment-list"
+import Post from "../components/post/post"
 
 const PostPage = props => {
   const posts =
     props.data && props.data.posts && mapEdgesToNodes(props.data.posts)
 
-  return (
-    <Layout>
-      {posts &&
-        posts.map(post => (
-          <>
-            <Byline
-              author={post.author}
-              taggedPersons={post.taggedPersons}
-              publishedDate={post.published}
-            />
-            <ImageCarousel images={post.images} />
-            <Controls />
-            <Likes persons={post.likes} />
-            <PostBody text={post.text} />
-            <CommentList comments={post.comments} />
-          </>
-        ))}
-    </Layout>
-  )
+  return <Layout>{posts && posts.map(post => <Post post={post} />)}</Layout>
 }
 
 export const query = graphql`
