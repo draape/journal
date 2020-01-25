45e39ba413bde72d3eed4956490a3545f3dd597c
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-01-26 00:10:21 +0100

    Added post component

---
+++ web/src/components/post/post.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+import Byline from "../byline/byline"
+import ImageCarousel from "../image-carousel/image-carousel"
+import Controls from "../controls/controls"
+import Likes from "../likes/likes"
+import PostBody from "../post-body/post-body"
+import CommentList from "../comment-list/comment-list"
+
+import "./post.scss"
+
+const Post = ({ post }) => (
+  <div className={"post"}>
+    <Byline
+      author={post.author}
+      taggedPersons={post.taggedPersons}
+      publishedDate={post.published}
+    />
+    <ImageCarousel images={post.images} />
+    <Controls />
+    <Likes persons={post.likes} />
+    <PostBody text={post.text} />
+    <CommentList comments={post.comments} />
+  </div>
+)
+
+export default Post
