61d2c2d68b88def7e9c3c6b4c68a6a1101aa9fab
Erik Heumrønningen <erik.heumronningen@creuna.no>
2021-01-01 23:55:23 +0100

    Remove like and comment icon. Use svgr to include icons. Animates download icon.

--- web/src/components/controls/controls.js
+++ web/src/components/controls/controls.js
@@ -1,32 +1,11 @@
 import React from "react"
-import cn from "classnames"
 
-import Icon from "components/icon"
+import { ReactComponent as DownloadIcon } from "static/download.svg"
 
-const Controls = ({ isLiked, hasComments }) => (
+const Controls = () => (
   <div className="controls">
-    <a
-      href="#like"
-      className={cn("controls__button", {
-        "controls__button--like-filled": isLiked,
-        "controls__button--like": !isLiked,
-      })}
-    >
-      {isLiked ? <Icon name="heart-filled" /> : <Icon name="heart" />}
-    </a>
-    <a
-      href="#comment"
-      className={cn("controls__button", "controls__button--comment", {
-        active: hasComments,
-      })}
-    >
-      <Icon name="comment" />
-    </a>
-    <a
-      href="#download"
-      className={cn("controls__button", "controls__button--download")}
-    >
-      <Icon name="download" />
+    <a href="#download" className="controls__download-button">
+      <DownloadIcon />
     </a>
   </div>
 )
