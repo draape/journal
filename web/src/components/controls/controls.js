c967708a971b30f34f27c764ea4f8f21529a1857
Erik Heumrønningen <erik.heumronningen@creuna.no>
2021-03-06 20:26:04 +0100

    Move the download icon to an overlay of the image.

--- web/src/components/controls/controls.js
+++ web/src/components/controls/controls.js
@@ -2,15 +2,12 @@ import React from "react"
 
 import DownloadIcon from "../../../static/download.svg"
 
-const Controls = ({ imageUrl }) => {
-  // console.log(imageUrl)
-  return (
-    <div className="controls">
-      <a href={imageUrl} className="controls__download-button" download>
-        <DownloadIcon />
-      </a>
-    </div>
-  )
-}
+const Controls = ({ imageUrl }) => (
+  <div className="controls">
+    <a href={imageUrl} className="controls__download-button" download>
+      <DownloadIcon />
+    </a>
+  </div>
+)
 
 export default Controls
